c666f5fc3042383b797e65545e35dc4305385a6a
Brian French <brian.french@nih.gov>
2019-12-13 20:13:33 -0500

    Adding the zip extension.

--- docker-build.sh
+++ docker-build.sh
@@ -3,9 +3,7 @@
 # Runs the commands to test the images.
 docker-compose -p ci build && \
 docker-compose -p ci up -d && \
-docker logs bionic-php7.2-cli && \
-docker logs bionic-php7.3-cli && \
-docker logs xenial-php7.2-cli && \
-docker logs xenial-php7.3-cli && \
+docker logs php7.2-cli && \
+docker logs php7.3-cli && \
 docker images && \
 docker ps -a
