814686f1be769c06d273f0410f5184b30ea37ea8
Brian French <fr3nch13@gmail.com>
2022-05-17 16:08:39 -0400

    removing 7.2 and 7.3

--- docker-build.sh
+++ docker-build.sh
@@ -5,10 +5,6 @@ docker stop $(docker ps -a -q)
 # Runs the commands to test the images.
 docker-compose -p ci build && \
 docker-compose -p ci up -d && \
-docker logs php7.2-cli && \
-docker logs php7.2-apache && \
-docker logs php7.3-cli && \
-docker logs php7.3-apache && \
 docker logs php7.4-cli && \
 docker logs php7.4-apache && \
 docker logs php8.0-cli && \
